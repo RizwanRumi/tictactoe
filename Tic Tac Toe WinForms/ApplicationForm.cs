36e17a9c74ba7ae55905fec7e074b478275c8b98
RizwanRumi <rizwan.cse33@gmail.com>
2023-11-25 11:24:12 +0100

    add Restartgame method

--- Tic Tac Toe WinForms/ApplicationForm.cs
+++ Tic Tac Toe WinForms/ApplicationForm.cs
@@ -2,9 +2,23 @@ namespace Tic_Tac_Toe_WinForms
 {
     public partial class ApplicationForm : Form
     {
+        public enum Player
+        {
+            X, O
+        }
+
+        Player currentPlayer;
+        Random random = new Random();
+        int playerWinCount = 0;
+        int cpuWinCount = 0;
+        List<Button> buttons;
+
+
+
         public ApplicationForm()
         {
             InitializeComponent();
+            RestartGame();
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -19,12 +33,12 @@ namespace Tic_Tac_Toe_WinForms
 
         private void PlayerClickButton(object sender, EventArgs e)
         {
-
+            
         }
 
         private void RestartGame(object sender, EventArgs e)
         {
-
+            RestartGame();
         }
 
         private void CheckGame()
@@ -34,7 +48,14 @@ namespace Tic_Tac_Toe_WinForms
 
         private void RestartGame()
         {
+            buttons = new List<Button> { button1, button2, button3, button4, button5, button6, button7, button8, button9 };
 
+            foreach(Button x in buttons) 
+            {
+                x.Enabled = true;
+                x.Text = "?";
+                x.BackColor = DefaultBackColor;
+            }
         }
     }
 }
