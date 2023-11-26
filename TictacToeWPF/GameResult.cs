2d44daf2ee575e683a00caae8426aff9980a28dd
RizwanRumi <rizwan.cse33@gmail.com>
2023-11-26 19:26:26 +0100

    add WinInfo class

---
+++ TictacToeWPF/GameResult.cs
@@ -0,0 +1,8 @@
+﻿namespace TicTacToeWPF
+{
+    public class GameResult
+    {
+        public Player Winner { get; set; }
+        public WinInfo WinInfo { get; set; }
+    }
+}
---
+++ TictacToeWPF/WinInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace TicTacToeWPF
+{
+    public class WinInfo
+    {
+        public WinType Type { get; set; }
+        public int Number { get; set; }      
+    }
+}
