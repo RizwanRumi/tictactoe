570c268964f6589cdfb08973fb4e89dc11db8d8f
RizwanRumi <rizwan.cse33@gmail.com>
2023-11-26 19:27:49 +0100

    add gameState class

---
+++ TictacToeWPF/GameState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TicTacToeWPF
+{
+    public class GameState
+    {
+        public Player[,] GameGrid {  get; private set; }
+        public Player CurrentPlayer { get; private set; }
+        public int TurnsPassed { get; private set; }
+        public bool GameOver { get; private set; }
+
+        public event Action<int, int> MoveMade;
+        public event Action<GameResult> GameEnded;
+        public event Action GameRestarted;
+    }
+}
